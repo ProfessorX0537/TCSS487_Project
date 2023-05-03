5a06d72509422c4897bf56c981389dbffab2f499
ProfessorX0537 <XMHines01@gmail.com>
2023-05-03 16:18:24 -0700

    Keccak-f sponge implementation.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,24 +14,144 @@ import java.util.Arrays;
  * @author Thomas Brookes
  */
 public class Main {
+    /************************************************************
+     *                Keccak-f Sponge Construction              *
+     ************************************************************/
 
-    public static void main(String[] args) {
-	    // 2^8 = 255, 2^16 = 65536, 2^3 = 16777216
+    /**
+     * byte[] which is the width of the Keccak-f sponge (1600 bits) where each
+     * lane in the sponge is a 64 bit word. There are 25 lanes which makes a
+     *  64 * 5 * 5 three-dimensional matrix which Keccak-f permutations will be
+     *  performed on.
+     */
+    private byte[] b = new byte[200];
 
-        byte[] b = rightEncode(BigInteger.valueOf(16777216));
-        byte[] c = leftEncode(BigInteger.valueOf(2));
-        byte d = (byte) 255;
-        byte[] e = {0,0};
+    /**
+     * The number of rounds performed in Keccak-f
+     */
+    private final int keccakfRnds = 24;
+
+    /**
+     * Value used to initialize SHA3
+     */
+    private final int SHAKE256 = 32;
+
+    /**
+     * Values used in multiple methods
+     */
+    private int pt, rsiz, mdlen;
 
 
-        System.out.println("reversed value of byte c: " + reverseBitsByte(d));
-        System.out.println("byte array of rightEncode: " + Arrays.toString(b));
-        System.out.println("byte array of leftEncode: " + Arrays.toString(c));
-        System.out.println("Concatenation of b and c (b || c): " + Arrays.toString(concat(b,c)));
-        System.out.println("encodeString(\"\"): " + Arrays.toString(encodeString(e)));
-        System.out.println("Representation of BigInteger as a byte array: " + Arrays.toString(bigIntToByteArray(16777215)));
+    /**
+     * The round constants defined by specification of which there are 24
+     */
+    private final long[] keccakfRndc = {
+            0x0000000000000001L, 0x0000000000008082L, 0x800000000000808aL,
+            0x8000000080008000L, 0x000000000000808bL, 0x0000000080000001L,
+            0x8000000080008081L, 0x8000000000008009L, 0x000000000000008aL,
+            0x0000000000000088L, 0x0000000080008009L, 0x000000008000000aL,
+            0x000000008000808bL, 0x800000000000008bL, 0x8000000000008089L,
+            0x8000000000008003L, 0x8000000000008002L, 0x8000000000000080L,
+            0x000000000000800aL, 0x800000008000000aL, 0x8000000080008081L,
+            0x8000000000008080L, 0x0000000080000001L, 0x8000000080008008L
+    };
 
+    /**
+     * Rotation offsets for the roh function.
+     */
+    private final int[] keccakfRotc = {
+            1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
+            27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44
+    };
+
+    /**
+     * The position for each word with respect to lane shifts in pi function
+     */
+    private  final int[] keccakfPilane = {
+            10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
+            15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1
+    };
+
+    /**
+     * Will perform some bit rotation on a given lane
+     * @param x lane
+     * @param y amount of rotation
+     * @return the lane rotated
+     */
+    private static long rotLane64(long x, int y) {
+        return (x << y) | (x >>> (64 - y));
     }
+
+    private void sha3Keccakf(byte[] v) {
+        long[] bc = new long[5];
+        long[] st = new long[25];
+        long t;
+
+        // endianess conversion. this is redundant on little-endian targets
+        for (int i = 0, j = 0; i < 25; i++, j += 8) {
+            st[i] = (((long)v[j + 0] & 0xFFL)      ) | (((long)v[j + 1] & 0xFFL) <<  8) |
+                    (((long)v[j + 2] & 0xFFL) << 16) | (((long)v[j + 3] & 0xFFL) << 24) |
+                    (((long)v[j + 4] & 0xFFL) << 32) | (((long)v[j + 5] & 0xFFL) << 40) |
+                    (((long)v[j + 6] & 0xFFL) << 48) | (((long)v[j + 7] & 0xFFL) << 56);
+        }
+
+        // actual iteration
+        for (int r = 0; r < keccakfRnds; r++) {
+
+            // Theta
+            for (int i = 0; i < 5; i++) {
+                bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
+            }
+
+            for (int i = 0; i < 5; i++) {
+                t = bc[(i + 4) % 5] ^ rotLane64(bc[(i + 1) % 5], 1);
+                for (int j = 0; j < 25; j += 5)
+                    st[j + i] ^= t;
+            }
+
+            // Rho Pi
+            t = st[1];
+            for (int i = 0; i < 24; i++) {
+                int j = keccakfPilane[i];
+                bc[0] = st[j];
+                st[j] = rotLane64(t, keccakfRotc[i]);
+                t = bc[0];
+            }
+
+            //  Chi
+            for (int j = 0; j < 25; j += 5) {
+                for (int i = 0; i < 5; i++)
+                    bc[i] = st[j + i];
+                for (int i = 0; i < 5; i++)
+                    st[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
+            }
+
+            //  Iota
+            st[0] ^= keccakfRndc[r];
+        }
+
+        // endianess conversion. this is redundant on little-endian targets
+        for (int i = 0, j = 0; i < 25; i++, j+=8) {
+            t = st[i];
+            v[0 + j] = (byte) (t & 0xFF);
+            v[1 + j] = (byte) ((t >> 8) & 0xFF);
+            v[2 + j] = (byte) ((t >> 16) & 0xFF);
+            v[3 + j] = (byte) ((t >> 24) & 0xFF);
+            v[4 + j] = (byte) ((t >> 32) & 0xFF);
+            v[5 + j] = (byte) ((t >> 40) & 0xFF);
+            v[6 + j] = (byte) ((t >> 48) & 0xFF);
+            v[7 + j] = (byte) ((t >> 56) & 0xFF);
+        }
+    }
+
+
+
+
+
+    /************************************************************
+     *                    Auxiliary Methods                     *
+     ************************************************************/
+
 
     /**
      * Encodes a BigInteger into a byte[] representation then reverses all the bits and appends the
@@ -166,6 +286,17 @@ public class Main {
     }
 
 
+
+
+
+
+
+
+    /************************************************************
+     *                      Helper Methods                      *
+     ************************************************************/
+
+
     /**
      * For debugging purposes. Accepts and int and coverts it to a BigInteger then uses
      * method available to BigInt to covert it to a byte[].
@@ -207,4 +338,29 @@ public class Main {
         System.arraycopy(b2,0,z,b1.length,b2.length);
         return z;
     }
+
+
+
+    /************************************************************
+     *                          Driver                          *
+     ************************************************************/
+
+    public static void main(String[] args) {
+        // 2^8 = 255, 2^16 = 65536, 2^3 = 16777216
+
+        byte[] b = rightEncode(BigInteger.valueOf(16777216));
+        byte[] c = leftEncode(BigInteger.valueOf(2));
+        byte d = (byte) 255;
+        byte[] e = {0,0};
+
+
+        System.out.println("reversed value of byte c: " + reverseBitsByte(d));
+        System.out.println("byte array of rightEncode: " + Arrays.toString(b));
+        System.out.println("byte array of leftEncode: " + Arrays.toString(c));
+        System.out.println("Concatenation of b and c (b || c): " + Arrays.toString(concat(b,c)));
+        System.out.println("encodeString(e): " + Arrays.toString(encodeString(e)));
+        System.out.println("Representation of BigInteger as a byte array: " + Arrays.toString(bigIntToByteArray(16777215)));
+
+    }
+
 }
