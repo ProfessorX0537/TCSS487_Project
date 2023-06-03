d152f326367072d6c59b9db4833ad38d6f717bdc
ProfessorX0537 <XMHines01@gmail.com>
2023-06-02 18:46:04 -0700

    Changes by cagep

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,10 @@
 package com.company;
 
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -42,10 +41,20 @@ public class Main {
         // 2^8 = 255, 2^16 = 65536, 2^3 = 16777216
 
         Scanner userIn = new Scanner(System.in);
-        do {
-            selectServicePrompt(userIn);
-        } while (repeat(userIn));
-        userIn.close();
+        int categoryResponse = selectCategoryPrompt(userIn);
+
+        switch (categoryResponse) {
+            case 1:
+                do {
+                    selectKMACServicePrompt(userIn);
+                } while (repeat(userIn));
+                userIn.close();
+            case 2:
+                do {
+                    selectECServicePrompt(userIn);
+                } while (repeat(userIn));
+                userIn.close();
+        }
 
         String data = "00 01 02 03";
         String n = "";
@@ -58,7 +67,21 @@ public class Main {
      *                          Prompts                          *
      *************************************************************/
 
-    private static void selectServicePrompt(final Scanner userIn) {
+    private static int selectCategoryPrompt(final Scanner userIn) {
+        String menuPrompt = """
+                Please enter the corresponding number of the category of service you would like to use:
+                    1) SHA-3 Derived Cryptographic Hashing
+                    2) Elliptic Curve Hashing
+                """;
+        int response = getIntInRange(userIn, menuPrompt, 1, 4);
+        if (response == 1) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    private static void selectKMACServicePrompt(final Scanner userIn) {
         String menuPrompt = """
                 Please enter the corresponding number of the service you would like to use:
                     1) Compute a plain cryptographic hash
@@ -68,13 +91,45 @@ public class Main {
                 """;
         int response = getIntInRange(userIn, menuPrompt, 1, 4);
         if (response == 1) {
-            plainHashService(fileOrInputPrompt(userIn));
+            plainHashServiceKMAC(fileOrInputPrompt(userIn));
         } else if (response == 2) {
-            authTagService(fileOrInputPrompt(userIn));
+            authTagServiceKMAC(fileOrInputPrompt(userIn));
         } else if (response == 3) {
-            encryptService();
+            encryptServiceKMAC();
         } else {
-            decryptService(decryptPreviousEncryptOrGivenCryptogram(userIn));
+            decryptServiceKMAC(decryptPreviousEncryptOrGivenCryptogram(userIn));
+        }
+    }
+
+    private static void selectECServicePrompt(final Scanner userIn) {
+        String menuPrompt = """
+                Please enter the corresponding number of the service you would like to use:
+                    1) Generate a public key to a file using an Elliptic Curve
+                    2) Encrypt a data file under a given elliptic public key file and write
+                       the ciphertext to a file
+                    3) Decrypt a given elliptic-encrypted file from a given password and
+                       write the decrypted data to a file
+                    4) Sign a given file from a given password and write the signature to
+                       a file
+                    5) Verify a given data file and its signature file under a given public
+                       key file
+                """;
+        int response = getIntInRange(userIn, menuPrompt, 1, 4);
+        if (response == 1) {
+            System.out.println("In 1");
+            keyPairEC();
+        } else if (response == 2) {
+            System.out.println("In 2");
+            encryptEC();
+        } else if (response == 3) {
+            System.out.println("In 3");
+            decryptEC();
+        } else if (response == 4) {
+            System.out.println("In 4");
+            signFileEC();
+        } else if (response == 5) {
+            System.out.println("In 5");
+            verifyFileEC();
         }
     }
 
@@ -122,7 +177,7 @@ public class Main {
     }
 
     /**************************************************************
-     *                          Services                          *
+     *                        KMAC Services                       *
      **************************************************************/
 
     /**
@@ -131,7 +186,7 @@ public class Main {
      * The user can choose between a file or command line for input.
      * @param input the input method, "file" for file input and "user input" for command line input.
      */
-    private static void plainHashService(final String input) {
+    private static void plainHashServiceKMAC(final String input) {
         //input will be "file" or "user input"
         byte[] byteArray;
         String theString = null;
@@ -157,7 +212,7 @@ public class Main {
      * The user can choose between a file or command line for input.
      * @param input the input method, "file" for file input and "user input" for command line input.
      */
-    private static void authTagService(final String input) {
+    private static void authTagServiceKMAC(final String input) {
         //input will be "file" or "user input"
         byte[] byteArray;
         String thePhrase = null;
@@ -184,7 +239,7 @@ public class Main {
      * Driver method for the encryption service.
      * Prints out an encrypted version for the given input file under a given passphrase.
      */
-    private static void encryptService() {
+    private static void encryptServiceKMAC() {
         Scanner userIn = new Scanner(System.in);
         File theFile = getUserInputFile(userIn);
         String theFileContent = fileToString(theFile);
@@ -192,7 +247,7 @@ public class Main {
         byte[] byteArray = theFileContent.getBytes();
         System.out.println("Please enter a passphrase: ");
         thePassphrase = userIn.nextLine();
-        prevEncrypt = encrypt(byteArray, thePassphrase);
+        prevEncrypt = encryptKMAC(byteArray, thePassphrase);
         System.out.println(bytesToHexString(prevEncrypt));
     }
 
@@ -200,19 +255,19 @@ public class Main {
      * Driver method for the decryption service.
      * Prints out a decrypted version for the given symmetric cryptogram under a given passphrase.
      */
-    private static void decryptService(String input) {
+    private static void decryptServiceKMAC(String input) {
         Scanner userIn = new Scanner(System.in);
         String thePassphrase;
         byte[] decryptedByteArray = new byte[0];
         System.out.println("Please enter a passphrase used to encrypt: ");
         thePassphrase = userIn.nextLine();
         if (input.equals("prev encrypt")) { //input from file
-            decryptedByteArray = decrypt(prevEncrypt, thePassphrase);
+            decryptedByteArray = decryptKMAC(prevEncrypt, thePassphrase);
         } else if (input.equals("user input")) { //input from command line
             System.out.println("\nPlease input a cryptogram in hex string format in one line (spaces okay, NO NEW LINES!!!!!): \n");
             String userString = userIn.nextLine();
             byte[] hexBytes = hexStringToBytes(userString);
-            decryptedByteArray = decrypt(hexBytes, thePassphrase);
+            decryptedByteArray = decryptKMAC(hexBytes, thePassphrase);
         }
         System.out.println("\nDecryption in Hex format:\n" + bytesToHexString(decryptedByteArray));
         System.out.println("\nDecryption in String format:\n" + new String (decryptedByteArray, StandardCharsets.UTF_8));
@@ -224,7 +279,7 @@ public class Main {
      * @param pw the passphrase given by the user.
      * @return an encrypted version of the given byte array.
      */
-    private static byte[] encrypt(byte[] m, String pw) {
+    private static byte[] encryptKMAC(byte[] m, String pw) {
         byte[] rand = new byte[64];
         z.nextBytes(rand);
 
@@ -248,7 +303,7 @@ public class Main {
      * @param pw the passphrase given by the user.
      * @return a decrypted version of the given cryptogram.
      */
-    private static byte[] decrypt(byte[] cryptogram, String pw) {
+    private static byte[] decryptKMAC(byte[] cryptogram, String pw) {
         byte[] rand = new byte[64];
         //retrieve 512-bit random number contacted to beginning of cryptogram
         System.arraycopy(cryptogram, 0, rand, 0, 64);
@@ -277,6 +332,233 @@ public class Main {
         else {
             throw new IllegalArgumentException("Tags didn't match");
         }
+    }
+
+    /**************************************************************
+     *                         EC Services                        *
+     **************************************************************/
+
+    private static void keyPairEC() {
+        System.out.println("uwu in key pair :3");
+
+        File publicKeyOutputFile = new File("PublicKeyOutputEC.txt");
+        File privateKeyOutputFile = new File("PrivateKeyOutputEC.txt");
+
+        Scanner userIn = new Scanner(System.in);
+        String thePassphrase;
+        System.out.println("Please enter a passphrase used to encrypt: ");
+        thePassphrase = userIn.nextLine();
+
+        //Generate the key pair from the passphrase
+
+        //s multiple of 4?
+        byte[] s = KMACXOF256(thePassphrase.getBytes(), "".getBytes(), 512, "SK".getBytes());
+        Point V = exponentiation(G, new BigInteger(s));
+
+        //Write public key to file
+        writeToOutputFile(publicKeyOutputFile, V.getPx() + ":" + V.getPy());
+        //Write private key to file
+        writeToOutputFile(privateKeyOutputFile, bytesToHexString(s));
+    }
+
+    private static void encryptEC() {
+        System.out.println("ooo encrypt me daddy");
+        Scanner userIn = new Scanner(System.in);
+        File inputFile;
+        File encryptOutputFile = new File("CiphertextOutput.txt");
+        String input;
+
+        //get input
+        switch (fileOrInputPrompt(userIn)) {
+            case "file":
+                System.out.println("Chosen File :]");
+                inputFile = getUserInputFile(userIn);
+                input = fileToString(inputFile);
+            case "user input":
+                System.out.println("Chosen user input :]");
+                System.out.println("Please input a message you would like to be encrypted:");
+                input = userIn.nextLine();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + fileOrInputPrompt(userIn));
+        }
+
+        //encrypt input data (Stored in "input" string)
+        byte[] m = hexStringToBytes(input);
+        //make sure k is multiple of 4
+        byte[] k = new byte[64];
+        z.nextBytes(k);
+
+        String VString = fileToString(new File("publicKeyOutputFile.txt"));
+
+        Scanner stringScanner = new Scanner(VString);
+        String str = stringScanner.next();
+        String[] strarr = str.split(":");
+        Point V = new Point(new BigInteger(strarr[0]), new BigInteger(strarr[0]));
+
+        Point W = exponentiation(V, new BigInteger(k));
+        Point Z = exponentiation(G, new BigInteger(k));
+
+        //squeeze bits from sponge
+        byte[] keka = KMACXOF256(W.getPx().toByteArray(), "".getBytes(), 1024, "PK".getBytes());
+        byte[] ke = new byte[64];
+        System.arraycopy(keka,0,ke,0,64);
+        byte[] ka = new byte[64];
+        System.arraycopy(keka, 64,ka,0,64);
+
+        byte[] c = KMACXOF256(ke, "".getBytes(), (m.length * 8), "PKE".getBytes());
+        c =  xorBytes(c, m);
+        byte[] t = KMACXOF256(ka, m, 512, "PKA".getBytes());
+
+        //write the cipertext to the output file
+        writeToOutputFile(encryptOutputFile, "" +bytesToHexString(Z.getPx().toByteArray())  + ":"
+                +bytesToHexString(Z.getPy().toByteArray())  + ":"
+                + bytesToHexString(c) + ":" + bytesToHexString(t));
+
+    }
+
+    private static void decryptEC() {
+        System.out.println("MM decrypt :P");
+        Scanner userIn = new Scanner(System.in);
+        File inputFile;
+        File outputFile = new File("DecryptedEC.txt");
+        String thePassphrase;
+        String inputFileContents;
+        String decryptedData;
+
+        //get file
+        inputFile = new File("CiphertextOutput.txt");
+
+        //get passphrase
+        System.out.println("Please enter a passphrase used to encrypt: ");
+        thePassphrase = userIn.nextLine();
+
+        //get file contents
+        inputFileContents = fileToString(inputFile);
+        String encryptedString = fileToString(new File("CiphertextOutput.txt"));
+        Scanner stringScanner = new Scanner(encryptedString);
+        String str = stringScanner.nextLine();
+        String[] strarr = str.split(":");
+        Point Z = new Point(new BigInteger(hexStringToBytes(strarr[0])), new BigInteger(hexStringToBytes(strarr[1])));
+        byte[] c = hexStringToBytes(strarr[2]);
+        byte[] t = hexStringToBytes(strarr[3]);
+
+        //decrypt file contents
+
+        //Make sure it is multiple of 4?
+        byte[] s = KMACXOF256(thePassphrase.getBytes(), "".getBytes(), 512, "SK".getBytes());
+
+        Point W = exponentiation(Z, new BigInteger(s));
+        //squeeze bits from sponge
+        byte[] keka = KMACXOF256(W.getPx().toByteArray(), "".getBytes(), 1024, "PK".getBytes());
+        byte[] ke = new byte[64];
+        System.arraycopy(keka,0,ke,0,64);
+        byte[] ka = new byte[64];
+        System.arraycopy(keka, 64,ka,0,64);
+
+        byte[] m = KMACXOF256(ke, "".getBytes(), (c.length*  8), "PKE".getBytes());
+        m = xorBytes(m, c);
+
+        byte[] tPrime = KMACXOF256(ka, m, 512, "PKA".getBytes());
+
+        if (Arrays.equals(t, tPrime)) {
+            writeToOutputFile(outputFile, bytesToHexString(m));
+        }
+        else {
+            throw new IllegalArgumentException("Tags didn't match");
+        }
+
+        //output decrypted contents
+//        writeToOutputFile(outputFile, DECRYPTED DATA STRING GOES HERE);
+    }
+
+    private static void signFileEC() {
+        System.out.println("sign FILE!");
+        Scanner userIn = new Scanner(System.in);
+        File inputFile;
+        File signedFile = new File("SignedInputSignature.txt");
+        String inputData;
+
+        //get input
+        switch (fileOrInputPrompt(userIn)) {
+            case "file":
+                System.out.println("Chosen File :]");
+                inputFile = getUserInputFile(userIn);
+                inputData = fileToString(inputFile);
+            case "user input":
+                System.out.println("Chosen user input :]");
+                System.out.println("Please input a message you would like to be encrypted:");
+                inputData = userIn.nextLine();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + fileOrInputPrompt(userIn));
+        }
+
+        System.out.println("Please enter a passphrase used to encrypt: ");
+        String thePassphrase = userIn.nextLine();
+
+        //sign input
+        //make sure it is a multiple of 4?
+        byte[] s = KMACXOF256(thePassphrase.getBytes(), "".getBytes(), 512, "SK".getBytes());
+        // make sure it is a multiple of 4?
+        byte[] k = KMACXOF256(s, inputData.getBytes(),512, "N".getBytes());
+        Point U = exponentiation(G, new BigInteger(k));
+
+        byte[] h = KMACXOF256(U.getPx().toByteArray(), inputData.getBytes() ,512, "T".getBytes());
+        byte[] z = (new BigInteger(k).subtract(new BigInteger(h).multiply(new BigInteger(s)))).mod(getR()).toByteArray();
+
+
+        //write signed input to file
+        writeToOutputFile(signedFile, "" + bytesToHexString(h) + ":" + bytesToHexString(z));
+
+    }
+
+    private static void verifyFileEC() {
+        System.out.println("in verify file");
+        Scanner userIn = new Scanner(System.in);
+        File dataFile;
+        File signatureFile;
+        File publicKeyFile;
+        String dataFileContents;
+        String signatureFileContents;
+
+
+        System.out.println("DATA FILE");
+        dataFile = getUserInputFile(userIn);
+
+        System.out.println("SIGNATURE FILE");
+//        signatureFile = getUserInputFile(userIn);
+        signatureFile = new File("SignedInputSignature.txt");
+        Scanner signedFileReader = null;
+        try {
+            signedFileReader = new Scanner(signatureFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        assert signedFileReader != null;
+        String str = signedFileReader.nextLine();
+        String[] strarr = str.split(":");
+        String hexStringH = strarr[0];
+        byte[] h = hexStringToBytes(hexStringH);
+        String hexStringZ = strarr[1];
+        byte[] z = hexStringToBytes(hexStringZ);
+
+        System.out.println("PUBLIC KEY FILE");
+        String VString = fileToString(new File("publicKeyOutputFile.txt"));
+        Scanner stringScanner = new Scanner(VString);
+        String str2 = stringScanner.nextLine();
+        String[] strarr2 = str2.split(":");
+        Point V = new Point(new BigInteger(strarr2[0]), new BigInteger(strarr2[1]));
+
+        //verify
+        Point U = add(exponentiation(G, new BigInteger(z)), exponentiation(V, new BigInteger(h)));
+        if (KMACXOF256(U.getPx().toByteArray(), fileToString(dataFile).getBytes(), 512, "T".getBytes()) == h) {
+            //successs
+            System.out.println("SUCCESSSSSSSSSSSSSSS VERIFICATION");
+        } else {
+            System.out.println("FAILUREEEEEEEEEEEEEe VERIFICATION");
+        }
+
     }
 
     /**************************************************************
@@ -358,6 +640,22 @@ public class Main {
             e.printStackTrace();
         }
         return theString;
+    }
+
+    /**
+     * Writes all the required information to the specified output file.
+     */
+    private static void writeToOutputFile(File outputFile, String contents) {
+        Scanner stringScan = new Scanner(contents);
+        try {
+            FileWriter fw = new FileWriter(outputFile);
+            while (stringScan.hasNextLine()) {
+                fw.write(stringScan.nextLine());
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
